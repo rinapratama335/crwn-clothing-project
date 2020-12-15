5ace28e309a1a46f968280161cec066de27406f8
irwantowibowotech <irwantowibowotech@gmail.com>
2020-12-15 09:56:40 +0700

    add fungsi navigasi menggunakan withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/hompage/homepage";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 };
